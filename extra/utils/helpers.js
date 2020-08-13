5db9fe65669a9cce2bb2b45aeb259bec922f1e26
PankajChibhrani-Successive <68905607+pankajchibhrani-successive@users.noreply.github.com>
2020-08-13 16:11:42 +0530

    Update helpers.js

--- extra/utils/helpers.js
+++ extra/utils/helpers.js
@@ -1,4 +1,5 @@
 
+    
     export function validateEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -10,10 +11,9 @@
     let a,b
    export function validateUsers(users){
 
-    for(let i=0;i<users.length;i++){
+    users.forEach((element) => {
 
-
-        [a,b]= [users[i].traineeEmail,users[i].reviewerEmail]  // destructring
+        [a,b]= [element.traineeEmail,element.reviewerEmail]  // destructring
 
         if(validateEmail(a)){
             validUsers++
@@ -29,8 +29,7 @@
         else{
             invalidUsers++
         }
-
-    }
+    });
 return {validUsers,invalidUsers}
    
 }
@@ -38,14 +37,14 @@ return {validUsers,invalidUsers}
 
 export default  function hasPermission(moduleName, role,permissionType,permission){
    
-    if(Object.keys(permission)[0]  == moduleName){
+    if(Object.keys(permission)[0]  === moduleName){
 
         if(permission[moduleName].hasOwnProperty(permissionType)){
 
-            if((permissionType == 'all' && permission[moduleName].all[0] == role)  ||
-            (permissionType == 'read' && (permission[moduleName].read[0] == role   || permission[moduleName].read[1] == role)) ||
-            (permissionType == 'write' && permission[moduleName].write[0]==role) ||
-            (permissionType == 'delete' &&    permission[moduleName].delete[0] == role))
+            if((permissionType === 'all' && permission[moduleName].all[0] === role)  ||
+            (permissionType === 'read' && (permission[moduleName].read[0] === role   || permission[moduleName].read[1] === role)) ||
+            (permissionType === 'write' && permission[moduleName].write[0]===role) ||
+            (permissionType === 'delete' &&    permission[moduleName].delete[0] === role))
            {
                return true
            }
